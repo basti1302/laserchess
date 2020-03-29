da6e0a7b88a4838de5f56311d7ccb05b9d00527a
Bastian Krol <bastian.krol@web.de>
2020-03-29 23:53:31 +0200

    feat: copy getting started tutorial (Tic Tac Toe)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Client} from 'boardgame.io/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import TicTacToeBoard from './TicTacToeBoard';
+
+const rowOffset = 3;
+
+// Return true if `cells` is in a winning configuration.
+function IsVictory(cells) {
+  for (let row = 0; row < 3; row++) {
+    if (
+      isThreeInARow(
+        getCell(cells, row, 0),
+        getCell(cells, row, 1),
+        getCell(cells, row, 2),
+      )
+    ) {
+      return true;
+    }
+  }
+  for (let col = 0; col < 3; col++) {
+    if (
+      isThreeInARow(
+        getCell(cells, 0, col),
+        getCell(cells, 1, col),
+        getCell(cells, 2, col),
+      )
+    ) {
+      return true;
+    }
+  }
+  if (
+    isThreeInARow(
+      getCell(cells, 0, 0),
+      getCell(cells, 1, 1),
+      getCell(cells, 2, 2),
+    )
+  ) {
+    return true;
+  }
+  return isThreeInARow(
+    getCell(cells, 0, 2),
+    getCell(cells, 1, 1),
+    getCell(cells, 2, 0),
   );
 }
+
+function getCell(cells, row, col) {
+  return cells[row * rowOffset + col];
+}
+
+function isThreeInARow(c1, c2, c3) {
+  return c1 != null && c1 === c2 && c2 === c3;
+}
+
+// Return true if all `cells` are occupied.
+function IsDraw(cells) {
+  return cells.filter((c) => c === null).length === 0;
+}
+
+const TicTacToe = {
+  setup: () => ({cells: Array(9).fill(null)}),
+
+  moves: {
+    clickCell: (G, ctx, id) => {
+      if (G.cells[id] === null) {
+        G.cells[id] = ctx.currentPlayer;
+      }
+    },
+  },
+
+  endIf: (G, ctx) => {
+    if (IsVictory(G.cells)) {
+      return {winner: ctx.currentPlayer};
+    }
+    if (IsDraw(G.cells)) {
+      return {draw: true};
+    }
+  },
+
+  turn: {
+    moveLimit: 1,
+  },
+
+  ai: {
+    enumerate: (G, ctx) => {
+      let moves = [];
+      for (let i = 0; i < 9; i++) {
+        if (G.cells[i] === null) {
+          moves.push({move: 'clickCell', args: [i]});
+        }
+      }
+      return moves;
+    },
+  },
+};
+
+const App = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+});
 
 export default App;
