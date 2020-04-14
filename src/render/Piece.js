6db2708f0f00dd03f5b7803ba8490351a0264283
Joshua Krol <joshua.krol@gmx.de>
2020-04-14 13:36:45 +0200

    Figuren und feldfarbe geändert

--- src/render/Piece.js
+++ src/render/Piece.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Piece.module.css';
+import styles from "./Piece.module.css";
 
 export default class Piece extends React.Component {
   render() {
     const classes = [
       styles.piece,
+      styles[this.props.piece.player.color.label],
       styles[this.props.piece.type.as(this.props.piece.player)],
       styles[this.props.piece.orientation.cssClass],
     ];
-    return <span className={classes.join(' ')} />;
+    return <span className={classes.join(" ")} />;
   }
 }
