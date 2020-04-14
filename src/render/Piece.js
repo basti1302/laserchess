a8485529c5083546307035cb6fb3e0823be3d979
Bastian Krol <bastian.krol@web.de>
2020-04-14 22:56:05 +0200

    feat(render): use rendered piece for 90-degree pawn and squares
    Also:
    - add basic Blender file for modelling/rendering pieces

--- src/render/Piece.js
+++ src/render/Piece.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import styles from "./Piece.module.css";
+import styles from './Piece.module.css';
 
 export default class Piece extends React.Component {
   render() {
@@ -10,6 +10,6 @@ export default class Piece extends React.Component {
       styles[this.props.piece.type.as(this.props.piece.player)],
       styles[this.props.piece.orientation.cssClass],
     ];
-    return <span className={classes.join(" ")} />;
+    return <div className={classes.join(' ')} />;
   }
 }
